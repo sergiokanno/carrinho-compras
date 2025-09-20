9650946949535e3a503c8c3ea8ae6724a0f73ee7
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-09-20 08:48:36 -0300

    Adicionando compras no carrinho

--- js/app.js
+++ js/app.js
@@ -3,20 +3,26 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-
-    alert(nomeProduto);
-    alert(quantidade.value);
+    let quantidade = document.getElementById('quantidade').value;
 
 //calcular o preço, o nosso subtotal
-    let preco = (valorUnitario * quantidade.value).toFixed(2);
-    alert(preco);
+    let preco = (valorUnitario * quantidade).toFixed(2);
+    let carrinho = document.getElementById('lista-produtos');
 
 //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
+    </section>`;
+
 //atualizar o valor total
 
 }
 
 function limpar() {
 
+}
+
+function limpar() {
+    document.getElementById('produto').value = '';
+    document.getElementById('quantidade').value = '';
 }
