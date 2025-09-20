fa56a5a8dfd72314fc7e60e28c749456963a065d
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-09-20 07:53:53 -0300

    Testando função adicionar - Recuperando nome do produto e quantidade

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function adicionar() {
+//recuperar valores nome do produto, quantidade e valor
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+//calcular o preço, o nosso subtotal
+//adicionar no carrinho
+//atualizar o valor total
+
+}
+
+function limpar() {
+
+}
