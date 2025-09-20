a026a61683eccf2b9f86ce5b92e01b1efdb9704d
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-09-20 08:23:47 -0300

    Testando função adicionar - realizando cálculos - obtendo subtotal

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 function adicionar() {
 //recuperar valores nome do produto, quantidade e valor
-    let nomeProduto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto.value);
+
+    alert(nomeProduto);
     alert(quantidade.value);
 
 //calcular o preço, o nosso subtotal
+    let preco = (valorUnitario * quantidade.value).toFixed(2);
+    alert(preco);
+
 //adicionar no carrinho
 //atualizar o valor total
 
